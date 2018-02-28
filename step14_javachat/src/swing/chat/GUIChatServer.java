c3a3b83747625ff37827ef919743f4f3c712f6be
susie <susie3961@naver.com>
2018-02-28 15:36:18 +0900

    ver2. ip접속(서버) 이시영

--- step14_javachat/src/swing/chat/GUIChatServer.java
+++ step14_javachat/src/swing/chat/GUIChatServer.java
@@ -1,11 +1,19 @@
 package swing.chat;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javax.swing.*;
 
 public class GUIChatServer extends JFrame implements ActionListener{
 	TextArea txt_list;
 	JButton btn_exit;
+	
+	//테스트1---------------------------------------
+	ServerSocket ss=null;
+	//테스트1---------------------------------------
 	
 	public GUIChatServer()
 	{
@@ -18,18 +26,34 @@ public class GUIChatServer extends JFrame implements ActionListener{
 		add(btn_exit,"South");
 		setSize(250,250);
 		setVisible(true);
+		
+		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//이벤트처리-----------------------
-		addWindowListener(
-				new WindowAdapter(){
-					public void windowClosing(WindowEvent e)
-					{
-						System.exit(0);
-					}
-				}
-			);
 		btn_exit.addActionListener(this);
 		//----------------------------------
+		
+		serverStart();
 	}
+	
+	//테스트1(준비단계:서버구동)-------------------------------------------
+	public void serverStart(){
+		final int PORT=7500;
+		
+		try{
+			ss=new ServerSocket(PORT);
+			while(true)
+			{
+				Socket sock=ss.accept();
+				String str=sock.getInetAddress().getHostAddress();
+				txt_list.append(str);
+				
+			}			
+		}catch(IOException e){
+			e.printStackTrace();
+		}		
+	}
+	//테스트1-----------------------------------------------------------------
+	
 	
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == btn_exit)
